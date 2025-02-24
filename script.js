d098e6402a3895193cbcef91c4efe639b974a23d
Marco-Rocco <marcorocco28@icloud.com>
2025-02-24 17:48:36 +0100

    feat: print passenger age

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ console.log('km=' + domandaKm)
 // ora, assegnamo un valore ad ogni chilometro percorso, così da avere un valore del viaggio iniziale
 const valoreViaggio = (domandaKm * 0.21 )
 console.log('Il valore iniziale del viaggio è' + ' ' + '€' + valoreViaggio)
+
+// creiamo un altro  prompt per chiedere l'età del viaggiatore
+const domandaEta = prompt("specifica la tua età in valore numerale, per favore")
+console.log("l'età del passeggero è" + ' ' + domandaEta + ' ' + "anni")
+
+// creiamo una condizione di esistente tra due fasce di età
+
+   
