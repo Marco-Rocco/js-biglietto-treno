21c895a393dfbd70fab3851af349819164046f43
Marco-Rocco <marcorocco28@icloud.com>
2025-02-24 19:07:29 +0100

    fix: reduce decimal points to max 2

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ const domandaKm = prompt ("Ciao, quanti chilometri (km) percorrerà il tuo treno
 console.log('km=' + domandaKm)
 
 // ora, assegnamo un valore ad ogni chilometro percorso, così da avere un valore del viaggio iniziale
-const valoreViaggio = (domandaKm * 0.21 )
-console.log('Il valore iniziale del viaggio è' + ' ' + '€' + valoreViaggio)
+let valoreViaggio = (domandaKm * 0.21 )
+console.log('Il valore iniziale del viaggio è' + ' ' + '€' + (valoreViaggio.toFixed(2)))
+
 
 // creiamo un altro  prompt per chiedere l'età del viaggiatore
 const domandaEta = prompt("specifica la tua età in valore numerale, per favore")
@@ -12,6 +13,12 @@ console.log("l'età del passeggero è" + ' ' + domandaEta + ' ' + "anni")
 
 // creiamo una condizione di esistente tra due fasce di età
 if (domandaEta < 18) {
-    console.log('prezzo finale del biglietto è' + ' ' + (valoreViaggio * 80 ) /100 )
+    console.log('prezzo finale del biglietto è' + ' ' + '€'+ ((valoreViaggio * 80 ) /100 ).toFixed(2))
+}
+ else if (domandaEta > 65) {
+    console.log('prezzo finale del biglietto è' + ' ' + '€'+ ((valoreViaggio * 60 ) /100 ).toFixed(2))
+}
+else {
+    console.log('prezzo finale del biglietto è' + ' ' + '€'+ valoreViaggio)
 }
    
